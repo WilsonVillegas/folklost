9b8fe67d3c5d8c3a725a901d8db8d84b617f8d7f
Wilson <wilsonvillegas@rocketmail.com>
2014-11-26 00:15:51 -0600

    added forest 2.3

---
+++ folklost/Assets/AxeController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AxeController : MonoBehaviour {
+
+	public bool axe = false;
+	public GameObject axemodel;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void AxeOnOff()
+	{
+		axemodel.SetActive(axe);
+	}
+
+	void OnMouseOver()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			axemodel.SetActive(!axe);
+			axe = !axe;
+		}
+	}
+}
---
+++ folklost/Assets/Scripts/ClickModelChange.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickModelChange : MonoBehaviour {
+
+	public Vector3[] positions;
+	public Vector3[] rotations;
+	public GameObject model;
+	private int index = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//model.transform.position = positions[0];
+		//model.transform.rotation.Set ( rotations[index].x, rotations[index].y, rotations[index].z, model.transform.rotation.w);
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseOver()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			animation.Play();
+			//model.transform.position = positions[index];
+		}
+	}
+}
